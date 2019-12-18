c4d8f63e535776fd6966e488d0f0a0a26da09f2f
subash <subash_thapa@gap.com>
2019-12-18 02:27:04 -0800

    Updated package and git ignore

--- Package.swift
+++ Package.swift
@@ -1,14 +1,15 @@
 // swift-tools-version:4.2
 // Generated automatically by Perfect Assistant
-// Date: 2019-12-14 15:30:12 +0000
+// Date: 2019-12-18 10:19:59 +0000
 import PackageDescription
 
 let package = Package(
 	name: "PerfectPhase1",
 	dependencies: [
-		.package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", "3.0.0"..<"4.0.0")
+		.package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", "3.0.0"..<"4.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", "3.0.0"..<"4.0.0")
 	],
 	targets: [
-		.target(name: "PerfectPhase1", dependencies: ["PerfectHTTPServer"]),
+		.target(name: "PerfectPhase1", dependencies: ["PerfectHTTPServer","PerfectMustache"])
 	]
 )
